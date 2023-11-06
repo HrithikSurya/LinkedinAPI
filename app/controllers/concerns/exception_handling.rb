2240b503adda6609f0e7f8372425b1d772932f90
HrithikSurya <hrithik@webkorps.com>
2023-11-07 04:16:09 +0530

    made separate exception concern

---
+++ app/controllers/concerns/exception_handling.rb
@@ -0,0 +1,23 @@
+module ExceptionHandling
+  extend ActiveSupport::Concern
+  
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: e, status:404
+    end
+    
+    rescue_from CanCan::AccessDenied do |exception|
+      render json: exception, status:401
+    end
+
+    rescue_from ActionController::RoutingError, with: :render_404
+    
+  end
+  
+  private
+
+  def render_404
+    render json: 'errors/404', status: 404
+  end
+
+end
